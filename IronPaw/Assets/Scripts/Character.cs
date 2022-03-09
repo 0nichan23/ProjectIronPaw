8b6cb3fb9bc71414d24669330703b9435936335b
On <ontamam@gmail.com>
2022-03-09 18:51:23 +0200

    stam

---
+++ IronPaw/Assets/Scripts/Character.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    List<Color> _colors;
+    int _maxHp;
+    int _currentHp;
+    int _currentBlock;
+    string _name;
+
+}
