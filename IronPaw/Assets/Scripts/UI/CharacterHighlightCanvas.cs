f1b5a9e9a78b2ee78aceb7a61e03c4477455f7b9
OhMyCodeIsTaken <ronbandelmusic@gmail.com>
2022-06-09 23:48:36 +0300

    Models now change on Character Highlight Canvas + Character name
    * Also Holy Strength description was changed

--- IronPaw/Assets/Scripts/UI/CharacterHighlightCanvas.cs
+++ IronPaw/Assets/Scripts/UI/CharacterHighlightCanvas.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class CharacterHighlightCanvas : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI _characterName;
     [SerializeField] private TextMeshProUGUI _passiveDesscription;
     [SerializeField] private TextMeshProUGUI _ultimateDesscription;
     [SerializeField] private TextMeshProUGUI _strengthText;
@@ -12,9 +13,10 @@ public class CharacterHighlightCanvas : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _dexterityText;
     [SerializeField] private TextMeshProUGUI _faithText;
 
-    [SerializeField] private Character _charTest;
+    [SerializeField] private List<GameObject> _characterModels;
     public void InitInfo(Character character)
     {
+        _characterName.text = character.CharacterName;
         _passiveDesscription.text = character.PassiveDescription;
         _ultimateDesscription.text = character.UltimateDescription;
 
@@ -22,5 +24,34 @@ public class CharacterHighlightCanvas : MonoBehaviour
         _intelligenceText.text = character.Stats.Intelligence.ToString();
         _dexterityText.text = character.Stats.Dexterity.ToString();
         _faithText.text = character.Stats.Faith.ToString();
+
+        TurnOnModel(character);
+    }
+
+    private void TurnOnModel(Character givenCharacter)
+    {
+        int index = 0;
+        List<Character> pointerList = null;
+
+        if(givenCharacter is Hero)
+        {
+            pointerList = PartyManager.Instance.Heroes;
+        }
+        else if (givenCharacter is Enemy)
+        {
+            pointerList = PartyManager.Instance.Enemies;
+        }
+
+        for (int i = 0; i < pointerList.Count; i++)
+        {
+            if (pointerList[i] == givenCharacter)
+            {
+                _characterModels[i].SetActive(true);
+            }
+            else
+            {
+                _characterModels[i].SetActive(false);
+            }
+        }
     }
 }
