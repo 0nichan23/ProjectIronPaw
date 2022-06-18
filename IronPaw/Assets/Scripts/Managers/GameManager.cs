2687ba854978f7675895cb5b636508f13a145955
OhMyCodeIsTaken <ronbandelmusic@gmail.com>
2022-06-19 01:09:15 +0300

    Set target framerate on update
    Rei Push

--- IronPaw/Assets/Scripts/Managers/GameManager.cs
+++ IronPaw/Assets/Scripts/Managers/GameManager.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
+    private void Update()
+    {
+        Application.targetFrameRate = 60;
+    }
+
     public void RestartGame()
     {
         SceneManager.LoadScene(0);
