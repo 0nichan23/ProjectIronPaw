b05fa36d3524fc3a24cd207707f3a03062e0de0d
OhMyCodeIsTaken <ronbandelmusic@gmail.com>
2022-07-31 20:13:33 +0300

    Arrow

--- IronPaw/Assets/Scripts/UI/CharacterSlot/FloatingArrow.cs
+++ IronPaw/Assets/Scripts/UI/CharacterSlot/FloatingArrow.cs
@@ -11,31 +11,11 @@ public class FloatingArrow : MonoBehaviour
 
     [SerializeField] private Color _startColor;
     [SerializeField] private Color _targetColor;
-    private float _elapsedTime;
-    float _timeToFlick = 0.25f;
+
+    [SerializeField] private float _timeToFlick;
 
     private void Update()
-    {
-        if(_isDimming)
-        {
-            _elapsedTime -= Time.deltaTime;
-            if(_elapsedTime < 0)
-            {
-                _isDimming = !_isDimming;
-            }
-           
-        }
-        else
-        {
-            _elapsedTime += Time.deltaTime;
-            if (_elapsedTime > _timeToFlick)
-            {
-                _isDimming = !_isDimming;
-            }
-        }
-
-        
-        //_arrowImage.color = Color.Lerp(_startColor, _targetColor, _elapsedTime);
-        _arrowImage.color = Color.Lerp(_startColor, _targetColor, Mathf.PingPong(Time.time, 1));
+    {        
+        _arrowImage.color = Color.Lerp(_startColor, _targetColor, Mathf.PingPong(Time.time, _timeToFlick));
     }
 }
